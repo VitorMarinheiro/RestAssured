8e7ca50e46134142add6240acd112bdf8bf4b886
vitormarinheiro <vitor.marinheiro16@gmail.com>
2021-10-05 23:14:55 -0300

    AWS Parameter Store Fixes.

--- src/main/java/header/HeaderCreator.java
+++ src/main/java/header/HeaderCreator.java
@@ -12,7 +12,13 @@ public class HeaderCreator {
 
     public static Map<String, String> headerWithAuthToken() {
         Map<String, String> header = new HashMap<>();
+
+//      Caso você não queira fazer uso do AWS Parameter Store, comente a linha de número 19,
+//      descomente a de número 20 e inclua sua chave de API no campo String.
+
         header.put("x-api-key", GetParameter.getApiToken());
+        //header.put("x-api-key", "SUA_CHAVE_DE_API_VEM_AQUI");
+
         return header;
     }
 
