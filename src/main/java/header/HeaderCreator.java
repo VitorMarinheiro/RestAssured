2fe7c23ba01889aa6bfcb089bdbc3024cd476ff2
vitormarinheiro <vitor.marinheiro16@gmail.com>
2021-10-06 00:49:04 -0300

    Editing AWS Parameter Store.

--- src/main/java/header/HeaderCreator.java
+++ src/main/java/header/HeaderCreator.java
@@ -11,10 +11,14 @@ public class HeaderCreator {
     }
 
     public static Map<String, String> headerWithAuthToken() {
-        Map<String, String> header = new HashMap<>();
 
-//      Caso você não queira fazer uso do AWS Parameter Store, comente a linha de número 19,
-//      descomente a de número 20 e inclua sua chave de API no campo String.
+        // If you don't want to make use of the AWS Parameter Store, comment out line number 23,
+        //uncomment the number 24 and include your API key in the String field.
+
+        //Caso você não queira fazer uso do AWS Parameter Store, comente a linha de número 23,
+        //descomente a de número 24 e inclua sua chave de API no campo String.
+
+        Map<String, String> header = new HashMap<>();
 
         header.put("x-api-key", GetParameter.getApiToken());
         //header.put("x-api-key", "SUA_CHAVE_DE_API_VEM_AQUI");
